e3381a2c7369703f15319795bed69c408b12ebe4
phuong-tanila <nguyenphuong040217@gmail.com>
2023-03-21 16:30:56 +0700

    add exception response, add login front-end

--- src/main/java/fa/training/frontend/controller/CoursesController.java
+++ src/main/java/fa/training/frontend/controller/CoursesController.java
@@ -1,5 +1,7 @@
 package fa.training.frontend.controller;
 
+import fa.training.frontend.helpers.JwtProvider;
+import io.jsonwebtoken.Claims;
 import model.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,26 +10,32 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
-import javax.websocket.server.PathParam;
 import java.util.List;
-
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.CookieValue;
 
 @Controller
 public class CoursesController {
+
     @Autowired
     private RestTemplate restTemplate;
     @Value("${courses.api.url}")
     private String apiUrl;
+
     @GetMapping("/home")
     public String home(Model model) {
         String url = apiUrl + "/courses/slider-popular";
         List<Course> courses = List.of(restTemplate.getForObject(url, Course[].class));
         model.addAttribute("courses", courses);
+//        restTemplate.
         String url1 = apiUrl + "/courses/slider-newest";
         List<Course> courses1 = List.of(restTemplate.getForObject(url1, Course[].class));
         model.addAttribute("courses1", courses1);
         return "home-page";
     }
+
     @GetMapping("/")
     public String index(Model model) {
         String url = apiUrl + "/courses";
@@ -38,13 +46,39 @@ public class CoursesController {
     }
 
     @GetMapping("/course-detail/{courseId}")
-    public String getCourseDetail(@PathVariable("courseId") int courseId, Model model){
-        String url = apiUrl  + "/courses/" + courseId;
+    public String getCourseDetail(
+            @PathVariable("courseId") int courseId,
+            Model model,
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+        String url = apiUrl + "/courses/" + courseId;
         Course course = restTemplate.getForObject(url, Course.class);
         model.addAttribute("course", course);
         String url1 = apiUrl + "/courses/slider-newest";
         List<Course> courses1 = List.of(restTemplate.getForObject(url1, Course[].class));
         model.addAttribute("courses1", courses1);
+
+        Cookie[] cookies = request.getCookies();
+        boolean foundTokenCookie = false;
+        String role = "US";
+        for (Cookie cooky : cookies) {
+            if (cooky.getName().equals("accessToken")) {
+                foundTokenCookie = true;
+                try {
+                    String accessToken = cooky.getValue();
+                    JwtProvider.validateAccessToken(accessToken);
+                    Claims claims = JwtProvider.getClaimsFromAccessToken(accessToken);
+                    role = claims.get("role").toString();
+                    
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                    cooky.setMaxAge(0);
+                }
+            }
+        }
+        model.addAttribute("role", role);
+        System.out.println(role);
         return "course-detail";
     }
 //    @PostMapping("/")
---
+++ src/main/java/model/ExceptionResponse.java
@@ -0,0 +1,15 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class ExceptionResponse {
+    public String exceptionType;
+    public String messages;
+}
