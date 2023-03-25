59150b85909f0d95a9789dd525c441b69cca8243
phuong-tanila <nguyenphuong040217@gmail.com>
2023-03-26 03:14:37 +0700

    add history

--- src/main/java/model/Order.java
+++ src/main/java/model/Order.java
@@ -2,6 +2,7 @@ package model;
 
 import lombok.*;
 
+import java.text.DecimalFormat;
 import java.util.Date;
 import java.util.Set;
 
@@ -14,8 +15,5 @@ public class Order {
     public Boolean paymentStatus;
     public String coupon;
     public String paymentId;
-    public User user;
     public Set<OrderDetail> orderDetails;
-
-
 }
