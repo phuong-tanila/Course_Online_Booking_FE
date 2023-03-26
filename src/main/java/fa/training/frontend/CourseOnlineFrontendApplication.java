10780da280ff8d8fe2fc34d7136959fab380d13f
HoaiPhuoGn <92800582+HoaiPhuoGn@users.noreply.github.com>
2023-03-26 21:52:40 +0700

    add views

--- src/main/java/fa/training/frontend/CourseOnlineFrontendApplication.java
+++ src/main/java/fa/training/frontend/CourseOnlineFrontendApplication.java
@@ -12,7 +12,6 @@ public class CourseOnlineFrontendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CourseOnlineFrontendApplication.class, args);
 	}
-
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
