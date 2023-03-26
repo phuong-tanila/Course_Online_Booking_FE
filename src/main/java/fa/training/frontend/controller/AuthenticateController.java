428e60513b209d0171dfc5ce1bfb8c18508b96b4
phuong-tanila <nguyenphuong040217@gmail.com>
2023-03-27 02:54:13 +0700

    add register

--- src/main/java/fa/training/frontend/controller/AuthenticateController.java
+++ src/main/java/fa/training/frontend/controller/AuthenticateController.java
@@ -6,29 +6,34 @@ package fa.training.frontend.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
-import fa.training.frontend.model.ExceptionResponse;
-import fa.training.frontend.model.LoginRequestModel;
-import fa.training.frontend.model.TokenAuthModel;
+
+import fa.training.frontend.model.*;
+
 import javax.servlet.http.Cookie;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.view.RedirectView;
 
 /**
- *
  * @author 15tha
  */
 @Controller
 public class AuthenticateController {
 
+    //    @Autowired
+//    PasswordEncoder passwordEncoder;
     @Autowired
     private RestTemplate restTemplate;
     @Value("${courses.api.url}")
@@ -48,21 +53,41 @@ public class AuthenticateController {
         if (tokenModel.getAccessToken() != null) {
             response.setStatus(HttpStatus.OK.value());
             response.getWriter().print(objectMapper.writeValueAsString(tokenModel));
-        }else{
+        } else {
             response.setStatus(HttpStatus.UNAUTHORIZED.value());
             ExceptionResponse res = new ExceptionResponse(
-                    "bad credentials!", 
+                    "bad credentials!",
                     "invalid email/phone or password");
             response.getWriter().print(objectMapper.writeValueAsString(res));
         }
     }
+
     @ResponseBody
-    void logout(@Validated @RequestBody TokenAuthModel tokenModel, HttpServletResponse response){
+    void logout(@Validated @RequestBody TokenAuthModel tokenModel, HttpServletResponse response) {
         Cookie refreshCookie = new Cookie("refreshToken", "");
         refreshCookie.setMaxAge(0);
         Cookie accessCookie = new Cookie("accessToken", "");
         refreshCookie.setMaxAge(0);
         response.addCookie(accessCookie);
         response.addCookie(refreshCookie);
+    }
+
+    @GetMapping("/register-form")
+    public String getForm(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public Object register(@ModelAttribute("user") User user, Model model) {
+        String url = apiUrl + "/auth/register";
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            ResponseEntity<User> response = restTemplate.postForEntity(url, user, User.class);
+            return new RedirectView("http://localhost:8083/home");
+        } catch (Exception ex) {
+            model.addAttribute("msg", "Email or phone number already exists");
+            return "register";
+        }
     }
 }
